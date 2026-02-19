e4bb5f439a34ad2207bf850d3aa586256a2500bd
tmartincic <martincic.tomas.hr@gmail.com>
2026-02-19 21:50:26 +0100

    add CI env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Set up test environment
-$dotenvEnv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../', '.env.test');
-$dotenvEnv->safeLoad();
+if (!getenv('CI')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../', '.env.test');
+    $dotenv->load();
+}
 
 require_once __DIR__ . '/../bin/setup.php';
